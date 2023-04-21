f36c3c0b2b97d423be78d7eb0437dbba7ecf01ac
Diego Noura de Brito <diego.nobri@gmail.com>
2023-04-21 00:26:11 -0300

    ajustes list especialidades

--- src/app/funcionario/especialidade/especialidade-list/especialidade-list.component.ts
+++ src/app/funcionario/especialidade/especialidade-list/especialidade-list.component.ts
@@ -5,18 +5,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-especialidade-list',
   templateUrl: './especialidade-list.component.html',
-  styleUrls: ['./especialidade-list.component.css']
+  styleUrls: ['./especialidade-list.component.css'],
 })
 export class EspecialidadeListComponent implements OnInit {
+  atualizandoLista: boolean = false;
   especialidades: Especialidade[] = [];
 
-  constructor(private service: EspecialidadeService) { }
+  constructor(private service: EspecialidadeService) {}
 
   ngOnInit(): void {
+    this.atualizar();
+  }
+
+  atualizar(): void {
+    this.atualizandoLista = true;
+    this.especialidades = [];
+
     this.service.listar().subscribe((lista) => {
-      console.log(lista)
+      this.atualizandoLista = false;
       this.especialidades = lista;
     });
   }
-
 }
