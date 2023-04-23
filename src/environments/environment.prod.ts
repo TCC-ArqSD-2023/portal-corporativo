4c5618e2bcc1aa9121f746291d0fe1c192cf2fc9
Diego Noura de Brito <diego.nobri@gmail.com>
2023-04-23 20:59:05 -0300

    configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   api: {
     baseUrl: 'http://gisa-apim.eastus.cloudapp.azure.com:9080'
+    // baseUrl: 'https://gisa-apim.eastus.cloudapp.azure.com:9443'
     // apiUrl: 'https://safapi.azurewebsites.net'
     // apiUrl: 'https://localhost:7299'
   }
